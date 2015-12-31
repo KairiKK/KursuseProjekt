30839063c29f6185bb52977c964b53a04eda28ed
KairiKK <kairi.kallas@hotmail.com>
2015-12-31 06:38:14 +0200

    otsast peale kõik, notepad's vt faili layouts

--- src/GUI/Reader.java
+++ src/GUI/Reader.java
@@ -1,30 +1,37 @@
 package GUI;
 
+import RSS_Connect.ConnectRSS;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.awt.event.ActionListener;
 import java.io.File;
+import java.util.Scanner;
 
 /**
  * Created by Kairi on 18.12.2015.
  */
 public class Reader extends Application {
-
+    Stage window;
+    Scene scene1, scene2;
 
     public static void main(String[] args) {
         launch(args);
@@ -32,95 +39,107 @@ public class Reader extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-          //  VBox vbox = new VBox();
-          //  Scene window = new Scene(vbox, 700, 500);
+        window = primaryStage;
 
-        // vbox.setSpacing(8);
-        // vbox.setBackground(2);
-        // vbox.backgroundProperty
-        // Color.rgb(255, 255, 255);
-        // window.setBackground(Color.rgb(22, 22, 33, 32));
+        //TOOLBAR on top
 
-        //button.setVisible(true);
+        //Button 1
+        Button button1 = new Button("Juhis");
+        button1.setOnAction(e -> window.setScene(scene2));
 
-            ChoiceBox valik = new ChoiceBox(FXCollections.observableArrayList(
-                    "Kanal 1", "Kanal 2", "Kanal 3"));
-
-
-            //vbox.getChildren().addAll(
-                    //valik
-          //  );
-
-           // primaryStage.setScene(window);
-            primaryStage.show();
-
-        // http://www.dummies.com/how-to/content/using-the-toolbar-class-in-javafx.html
-        Button btn1 = new Button("Sisesta");
-        Button btn2 = new Button("Kustuta");
-        Button btn3 = new Button("Eralda");
-       // btn1.setFont(Font.font("Harlow Solid Italic", FontWeight.BOLD, 12));
+        //Checkbox - actions to add
         CheckBox chk1 = new CheckBox("Teavita");
         CheckBox chk2 = new CheckBox("R");
-        StackPane feed = new StackPane();
-        feed.setAlignment(Pos.TOP_RIGHT);
-        feed.setPrefSize(100, 100);
 
+       // Button add feed - action to add (Dialogbox, JavaFX8 peaks toetama)
+        Button addfeed = new Button("+");                   //addfeed.setOnAction(event -> popuppeakstulemasiia);
+
+        //Choicebox
+        ChoiceBox choosefeed = new ChoiceBox(FXCollections.observableArrayList(
+                "Kanal 1", "Kanal 2", "Kanal 3"));
+
+        //Toolbar
         ToolBar toolBar1 = new ToolBar();
         toolBar1.getItems().addAll(
                 new Separator(),
-                btn1,
-                btn2,
-                btn3,
+                button1,
                 new Separator(),
                 new Separator(),
                 chk1,
                 chk2,
-                new Separator()
+                new Separator(),
+                addfeed,
+                choosefeed
+
         );
-        final BorderPane pane = new BorderPane();
 
-        pane.setTop(toolBar1);
-        pane.setRight(valik);
-        pane.setLeft(feed);
+        // SCROLLPANE ON THE RIGHT
 
-        final Scene scene = new Scene(pane, 600, 400);
-        pane.setStyle("-fx-background-color: rgb(56, 64, 90)");
-        toolBar1.setStyle("-fx-background-color: rgb(255, 255, 255)");
-        //btn1.setFont(Font.font("Harlow Solid Italic", FontWeight.BOLD, 12));
-       // scene.setFill(Color.WHITE);
+        Rectangle rect = new Rectangle(200, 400, Color.BLUEVIOLET);
+        ScrollPane s1 = new ScrollPane();
+        s1.setPrefSize(120, 120);
+        s1.setContent(rect);
 
-        valik.setPrefSize(200, 25);
-        valik.setStyle("-fx-background-color: rgb(255, 255, 255)");
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Fiider");
-        primaryStage.show();
+        // method that adds buttons and things alltoghther to borderpane
+        BorderPane borderpane = new BorderPane();
+            borderpane.setTop(toolBar1);
+            borderpane.setRight(rect);
+
+        scene1 = new Scene(borderpane, 600, 400);
+
+
+        //"FEEDWINDOW" CENTER, at least 3 different sections
+
+
+
+
+
+
+        //SCENE 2
+
+        //Button 2
+        Button button2 = new Button("Tagasi scene 1");
+        button2.setOnAction(e -> window.setScene(scene1));
+        button2.setAlignment(Pos.BOTTOM_CENTER);
+
+        Label label = new Label("Juhised on siin");
+        label.setAlignment(Pos.TOP_RIGHT);
+        label.setFont(new Font("Harlow Solid Italic", 24));//font
+
+        // tules teha ümber selliseks http://stackoverflow.com/questions/16828341/how-to-justify-text-for-a-character-array-in-java
+        Text text = new Text("SIIA TULEB PALJU TEKSTi" +  "\n" + "\n" +
+                "RSS (Rich Site Summary; originally RDF Site Summary; often called Really Simple Syndication) uses" +
+                "a family of standard web feed formats[2] to publish frequently updated information: blog entries, " +
+                "news headlines, audio, video. An RSS document (called \"feed\", \"web feed\",[3] or \"channel\") " +
+                "includes full or summarized text, and metadata, like publishing date and author's name.\n" +
+                "RSS feeds enable publishers to syndicate data automatically. A standard XML file format ensures " +
+                "compatibility with many different machines/programs. RSS feeds also benefit users who want to receive" +
+                " timely updates from favourite websites or to aggregate data from many sites.\n" +
+                "\n" +
+                "Subscribing to a website RSS removes the need for the user to manually check the website for new content. " +
+                "Instead, their browser constantly monitors the site and informs the user of any updates. The browser can " +
+                "also be commanded to automatically download the new data for the user.\n" +
+                "\n" +
+                "Software termed \"RSS reader\", \"aggregator\", or \"feed reader\", which can be web-based, desktop-based, " +
+                "or mobile-device-based, presents RSS feed data to users. Users subscribe to feeds either by entering " +
+                "a feed's URI into the reader or by clicking on the browser's feed icon. The RSS reader checks" +
+                " the user's feeds regularly for new information and can automatically download it, if that function" +
+                " is enabled. The reader also provides a user interface.");
+
+        VBox vboxjuhis = new VBox();
+        vboxjuhis.getChildren().addAll(label, text,button2);
+
+        scene2 = new Scene(vboxjuhis, 300, 300);
+
+
+
+        //KUVAMINE
+
+
+        //Display scene 1 at first
+        window.setScene(scene1);
+        window.setTitle("Fiider");
+        window.show();
     }
+
 }
-
-        //getContentPane().setBackground();
-        /*btn1.setOnAction((event) -> {
-        public void handle(ActionEvent e) { fileChooser.showOpenDialog(primaryStage);
-
-        // Siia tegevus, kui vajutad nuppu
-        }
-        });
-       // private Color background = new Color(55, 55, 55);
-       // private Color textcolor = new Color(255, 255, 255);
-
-
-/* static ObservableList toonid = // menyy
-            FXCollections.observableArrayList();
-    toonid.addAll(
-            "kollane, roosa, sinine, must"
-            );
-    list.setPrefWidth(100);
-    list.setPrefHeight(70);
-
-
-   */
-
-
-/*
-How to get data stored in array after application has ended?
-http://stackoverflow.com/questions/18827862/how-to-keep-data-stored-in-array-after-application-has-ended
-*/
--- src/RSS_Connect/ConnectRSS.java
+++ src/RSS_Connect/ConnectRSS.java
@@ -1,26 +1,116 @@
 package RSS_Connect;
 
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Created by Kairi on 19.12.2015.
  *
- * RSS feed connection jsoup abil
+ * RSS feed connection jsoup abil  PEAKS VÕTMA KÕIK LAHTI. TEEB SELLEST ARRAY LISTI JA SIIS TULEB PASSIDA WINDOW CLASSI EDAST JA TEEB NÄHTAVAKS
  * http://jsoup.org/cookbook/
  */
 public class ConnectRSS {
 
-    private Elements title, link, description;
+    private Document doc;
+    private Elements items, title, link, description;
+    private ArrayList titleString = new ArrayList();
+    private ArrayList<ArrayList<String>> newsItem = new ArrayList<ArrayList<String>>();
 
-        public static void main(String[] args) throws IOException {
 
-            String url = "http://www.delfi.ee/";
-            Document doc = Jsoup.connect(url).get();
+    public ConnectRSS(String feed) {
 
+        try {
+
+            Document doc = Jsoup.connect(feed).get(); // By the help of Jsoup.connect() method, we will connect with the URL.
+            items = doc.select("item");
+            System.out.println("[+] Connected to " + feed);
+
+
+            for (Element element : items) {
+
+
+                ArrayList<String> buffer = new ArrayList<String>();
+                title = element.select("title");
+                description = element.select("description");
+                link = element.select("link");
+                titleString.add((String) title.text());
+                buffer.add((String) title.text());
+                buffer.add((String) description.text());
+                buffer.add((String) link.text());
+                newsItem.add(buffer);
+            }
+
+        } catch (Exception e) {
+            System.out.println("[-] Errooor");
+        }
+
+        System.out.println("JESSSS");
+    }
+
+
+    public boolean testConnection(String feed) {
+
+        try {
+            doc = Jsoup.connect(feed).get();
+        } catch (Exception e) {
+            System.out.println("Boooo " + feed + " failed.");
+            return false;
+        }
+        System.out.println("Wooop " + feed + " successful");
+        return true;
+    }
+
+    public ArrayList<String> getTitle() {
+        return titleString;
+
+    }
+}
+
+class feed {
+
+    String title;
+    String description;
+    String link;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+
+    @Override
+    public String toString() {
+        return "feed [title=" + title + ", description=" + description
+                + ", link=" + link + "]";
+    }
+
+}
+        /*
             Elements links = doc.select("link[type=application/rss+html+xml]");
 
             while (links.size() > 0) {
@@ -41,3 +131,5 @@ public class ConnectRSS {
         return description;
     }
 }
+
+*/
